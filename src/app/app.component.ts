c3f4623917677eb59a325ed6a4aa55040f1660f9
Jimmy <chen.jimmym@gmail.com>
2017-11-06 12:31:16 -0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  // boxClicked: boolean = false;
-  gotClicked () {
-    boxClicked = true;
+  time = new Date();
+  selected = null;
+  gotClicked (timezone) {
+    this.time = new Date()
+    if (timezone == 'PST') {
+      console.log("PST");
+      this.selected = "PST";
+    }
+    if (timezone == 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+      console.log("MST");
+      this.selected = "MST";
+    }
+    if (timezone == 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+      console.log("CST");
+      this.selected = "CST";
+    }
+    if (timezone == 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+      console.log("EST");
+      this.selected = "EST";
+    }
+    if (timezone == 'CLEAR') {
+      this.time.setHours(this.time.getHours());
+      console.log("clear");
+      this.selected = "CLEAR";
+    }
   }
 }
